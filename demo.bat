0ee8b3d713949a877a59ec6e7159c4b0b8975820
Toshiyuki Masui <masui@pitecan.com>
2014-10-07 15:16:45 +0900

    パスを考えなくても動くようにdemo.batを修正

--- demo.bat
+++ demo.bat
@@ -1,2 +1,1 @@
-start "" "\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files "file:///C:/Users/masui/Downloads/Gear/Gear/demo.html"
-
+start "" chrome.exe --allow-file-access-from-files file:///%CD%\demo.html
